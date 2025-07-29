763af67d65f08a4daf96d700d5193791dc9c76ae
Tinchu <tinchuwork@gmail.com>
2025-07-29 20:57:26 +0530

    Updated install.sh script

--- install.sh
+++ install.sh
@@ -29,6 +29,18 @@ version_lt()
     [ "$(printf '%s\n' "$v1" "$v2" | sort -V | head -n 1)" != "$v2" ]
 }
 
+install_required_software()
+{
+    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+    cd nerd-fonts
+    ./install.sh
+
+    cd ..
+    rm -rf nerd-fonts
+
+    sudo apt install ripgrep
+}
+
 # MAIN
 echo "[+] 🔍 Checking current Neovim version ..."
 CURRENT_VERSION=$(installed_ver_chk)
@@ -39,15 +51,27 @@ REMOTE_REPO_VERSION=$(remote_ver_chk)
 echo "[-] Remote repo version : $REMOTE_REPO_VERSION"
 
 if version_lt "$CURRENT_VERSION" "$REMOTE_REPO_VERSION"; then
-    echo "Need to insall new version $REMOTE_REPO_VERSION"
+    install_required_software()
+    echo "⚠️ Need to insall new version $REMOTE_REPO_VERSION"
+    echo "  Downloading github repo"
+    git clone $NVIM_REPO_URL
+    cd neovim
+    git checkout $BRANCH   # Use latest stable version
+    echo "[+] 📦 Installing  neovim"
+    make CMAKE_BUILD_TYPE=Debug
+    sudo make install
 else
-    echo "NVIM is updated"
+    echo "✅  NVIM is updated"
 fi
 
-#version_lt "$CURRENT_VERSION" "$REMOTE_REPO_VERSION" && echo "Yes, Upgrade needed"
-#compare_versions "$CURRENT_VERSION" "$REMOTE_REPO_VERSION" && echo "Yes, Upgrade needed"
-#version_lt "$CURRENT_VERSION" "$REMOTE_REPO_VERSION" && echo "v0.12.0 < v0.11.3" || echo "v0.12.0 >= v0.11.3"
-#version_lt 81 23 && echo "81 < 23" || echo "81 >= 23"
+#Backup existing config
+if [ -d "$HOME/.config/nvim" ]; then
+    mv -f "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)"
+    echo "[-] ⚠️ Existing config backed up"
+fi
+
+git clone https://github.com/Tinchu20/nvim-cfg ~/.config/nvim
+echo "[+] ✅ Done. Launch Neovim with: nvim"
 
 echo " Neovim"   # nf-dev-neovim
 echo " GitHub"   # nf-fa-github
@@ -58,7 +82,6 @@ echo -e "\ue62b Neovim"
 echo -e "\ue7c5 Neovim"
 echo -e "\e[1;32m\ue7c5 Neovim\e[0m"
 
-
 echo "✅ Success"
 echo "⚠️ Warning"
 echo "❌ Failed"
@@ -66,10 +89,5 @@ echo "📦 Installing package"
 echo "🔍 Checking versions..."
 
 
-#echo "[+] Installing neovim"
 
-#Backup existing config
-#if [ -d "$HOME/.config/nvim" ]; then
-    #mv -f "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)"
-    #echo "[-] Existing config backed up"
-#fi
+
