f601ce9b9c5a4112d9283e95639711979d5d3acb
Tinchu <tinchuwork@gmail.com>
2026-09-06 21:37:49 +0530

    treesitter.lua updated

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+--[[
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -10,4 +11,85 @@ return {
             indent = {enable = true},
         })
     end
+}
+]]
+
+--[[
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        vim.filetype.add({
+            extension = {
+                slint = "slint",
+            },
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                local ft = vim.bo[args.buf].filetype
+                local lang = vim.treesitter.language.get_lang(ft)
+
+                if not lang then
+                    return
+                end
+
+                ts.install({ lang }):wait(30000)
+
+                if vim.api.nvim_buf_is_valid(args.buf) then
+                    pcall(vim.treesitter.start, args.buf, lang)
+                end
+            end,
+        })
+    end,
+}
+]]
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        vim.filetype.add({
+            extension = {
+                slint = "slint",
+            },
+        })
+
+        -- Install these once.
+        ts.install({
+            "asm",
+            "bash",
+            "c",
+            "cpp",
+            "lua",
+            "make",
+            "vim",
+            "rust",
+            "slint",
+        })
+
+        -- Start Treesitter when entering a supported filetype.
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                local ft = vim.bo[args.buf].filetype
+                local lang = vim.treesitter.language.get_lang(ft)
+
+                if not lang then
+                    return
+                end
+
+                if vim.api.nvim_buf_is_valid(args.buf) then
+                    pcall(vim.treesitter.start, args.buf, lang)
+                end
+            end,
+        })
+    end,
 }
